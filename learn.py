d3778edb7412f3043c6c999db0bef2a2233af452
Madi-S <khovansky99@gmail.com>
2024-08-12 13:36:29 +0500

    bigram.py -> learn.py

--- learn.py
+++ learn.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyper parameters
-batch_size = 64
-block_size = 256
-max_iters = 5000
-eval_interval = 500
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
